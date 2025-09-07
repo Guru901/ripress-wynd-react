d622efe87a3d0e6b280586b1ee9bc1f42dd236d2
Gurvinder Singh <avinashkaur842@gmail.com>
2025-09-07 19:54:18 +0530

    Add WebSocket support and update dependencies
    - Implemented WebSocket connection in the frontend to handle real-time messaging.
    - Updated `Cargo.toml` to include `wynd` for WebSocket handling and modified `ripress` dependency features.
    - Enhanced `Cargo.lock` with new dependencies including `base64`, `block-buffer`, `cpufeatures`, `digest`, `hyper-tungstenite`, `tokio-tungstenite`, and others.
    - Updated the backend to integrate `wynd` for WebSocket management and echoing messages back to clients.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,19 +3,39 @@ import "./App.css";
 
 function App() {
   const [hello, setHello] = useState<any>(null);
+  const [input, setInput] = useState("");
+  const [ws, setWs] = useState<WebSocket>();
+  const [messages, setMessages] = useState<string[]>([]);
 
   useEffect(() => {
     (async () => {
       const response = await fetch("/hello");
+      const ws = new WebSocket("ws://localhost:3000/ws");
 
       if (!response.ok) {
         throw new Error("Failed to fetch");
       }
 
-      const data = await response.json();
+      const data = await response.text();
+      console.log(data);
 
       setHello(data);
+
+      ws.onopen = () => {
+        console.log("WebSocket connected");
+        setWs(ws);
+      };
+
+      ws.onmessage = (event) => {
+        setMessages((prev) => [...prev, event.data]);
+      };
     })();
+
+    return () => {
+      if (ws) {
+        ws.close();
+      }
+    };
   }, []);
 
   return (
@@ -24,6 +44,22 @@ function App() {
       <div>
         <h2>/hello</h2>
         <pre>{hello ? JSON.stringify(hello, null, 2) : "Loading..."}</pre>
+      </div>
+      <div>
+        <h2>Websocket</h2>
+        <input
+          type="text"
+          placeholder="Type something..."
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={() => ws?.send(input)}>Send</button>
+
+        <p>Messages:</p>
+        <ul style={{ listStyle: "none", padding: 0 }}>
+          {messages.map((message, index) => (
+            <li key={index}>{message}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
